95683ad5fa7ad2796f56710f89163f41fc322a92
nithya-pk <105173126+nithya-pk@users.noreply.github.com>
2023-03-30 08:45:16 +0200

    Delete vars.tf
    Removing old file

--- vars.tf
+++
@@ -1,9 +0,0 @@
-variable "NK_VPC_cidr" {}
-variable "public_subnets" {}
-variable "private_subnets" {}
-
-variable "ssh_key_pair" {
- type        = string
- description = "SSH key pair to be provisioned on the instance"
- default     = null
-}
